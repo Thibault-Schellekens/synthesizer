da4528c7ee1b1effe7038a5a2da4bf8960e86a6e
63491 <63491@etu.he2b.be>
2025-05-23 13:09:11 +0200

    Add: Audio parameters

---
+++ synth/model/Parameters.h
@@ -0,0 +1,26 @@
+//
+// Created by schel.
+//
+
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+enum class Waveform {
+    SINE, SQUARE, SAW
+};
+
+struct Parameters {
+    bool osc1Enabled {true};
+    Waveform osc1Waveform {Waveform::SINE};
+    float osc1FrequencyOffset {0.0f};
+    bool osc2Enabled {true};
+    float attackTime {0.0f};
+    float releaseTime {0.0f};
+    float filterCutoff {20000.0f};
+    float filterResonance {0.0f};
+    float delayTime {0.1f};
+    float delayMix {0.0f};
+
+};
+
+#endif //PARAMETERS_H
